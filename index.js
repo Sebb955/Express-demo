097c2086e09ddbc530e239f6cfc8483627005443
Seb <79416415+Sebb955@users.noreply.github.com>
2021-12-19 18:32:02 +0100

    Add files via upload
    + node_modules
    
    * npm install init -y
    
    * npm install express pug

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
+
+const courses = [{ id: 1, name: 'HTML' }, { id: 2, name: 'CSS' }, { id: 3, name: 'JS' },
+    { id: 4, name: 'Node' }, { id: 5, name: 'Express' }
+];
+
+app.get('/courses/', (req, res) => {
+    res.render('index', { courses });
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
